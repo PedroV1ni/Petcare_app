44f4e632e03476ea9e809ef48ab1a05a5e18c708
CDMziin <pedrovinis13@gmail.com>
2026-08-25 03:08:01 -0300

    Corrige build Android e remove codigo morto
    O projeto nao compilava para Android. Duas causas independentes:
    
    1. data_service.dart chamava Breed.fromFirestore(), metodo que deixou
       de existir quando as racas migraram para JSON. O streamBreeds() era
       codigo morto - nenhuma tela o usava, breed_list le do asset. Removido
       junto com o import que ficou orfao.
    
    2. A toolchain Android estava atras dos plugins Flutter:
       - Kotlin 1.8.22 -> 2.1.0, pois url_launcher_android 6.3.30 usa o DSL
         kotlin { compilerOptions { } }, disponivel so a partir do Kotlin 2.0
       - AGP 8.7.0 -> 8.9.1, exigido por androidx.core 1.17 e androidx.browser 1.9
       - google-services 4.3.15 -> 4.4.2
       - Gradle 8.10.2 -> 8.11.1, minimo do AGP 8.9
    
    Verificado: flutter analyze sem erros, build web e APK debug gerados,
    app rodando no emulador com as colecoes news e care vindas do Firestore.

--- petcare_app/lib/services/data_service.dart
+++ petcare_app/lib/services/data_service.dart
@@ -1,16 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import '../models/breed.dart';
 import '../models/news.dart';
 import '../models/care.dart';
 
 class DataService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
-
-  Stream<List<Breed>> streamBreeds() => _db
-      .collection('breeds')
-      .orderBy('name')
-      .snapshots()
-      .map((snap) => snap.docs.map((d) => Breed.fromFirestore(d)).toList());
 
   Stream<List<News>> streamNews() => _db
       .collection('news')
